f41a27bc6a1d05b31ac636b816e5dafd3465e21d
Griffins Kiptanui Lelgut <134501193+Griffins2005@users.noreply.github.com>
2024-11-04 15:28:16 +0000

    cookie-banner

---
+++ scripts/gallery.js
@@ -0,0 +1,9 @@
+S("#buttonB").on("click", function () {
+    S("itemA").addClass("hidden");
+    S("itemB").removeClass("hidden");
+    S("itemA").addClass("hidden");
+
+    S("buttonA").removeClassClass("active-thumbnail");
+    S("buttonB").addClass("active-thumbnail");
+    S("buttonC").removeClassClass("active-thumbnail");
+})
